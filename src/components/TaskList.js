238cc38dc40b524c9788e19509567395fb97189a
Nicholas <nkotsakis13@gmail.com>
2021-07-17 14:20:34 -0400

    Did soething

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import Task from './task';
+import moment from 'moment';
 
 const TaskList = props => {
 
+    let now = new moment();
+    let dt = new Date();
+    let diff = 0;
+    var current = null;
+   
     if (props.tasks != null) {
         const taskCards = props.tasks.map(task => {
-            console.log(task.taskName);
+            
+            dt = new moment(task.dueDate.$date);
+            diff = dt.startOf('day').diff(now.startOf('day'),'days');
+            var text = '';
+
+            if(diff < 0 && current != -1){
+                current = -1;
+                text = 'Late';
+            } else if(diff == 0 && current != 0){
+                current = diff;
+                text = 'Today';
+            } else if(diff == 1 && current != 1){
+                current = 1;
+                text = 'Tommorrow';
+            } else if(diff < 7 && diff > 0 && current != 7){
+                current = 7;
+                text = 'This week';
+            } 
+
+            //console.log(a.diff(b, 'days'));
+            console.log(diff);
+            //console.log(moment(task.dueDate).format("MM/DD/YYYY"));
+
             return (
-                <Task key={task._id.$oid} task={task}/>
+                <div>
+                     <div class="sectiontitle">{text}</div>
+                    <Task key={task._id.$oid} task={task} />
+                </div>
             )
         });
         return <div>{taskCards}</div>
